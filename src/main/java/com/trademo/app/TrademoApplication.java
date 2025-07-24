c87cfc953ab94f706d87345994dce0977617acd1
sourav1321st <souravmeher983@gmail.com>
2025-07-25 03:27:00 +0530

    Initial commit

---
+++ src/main/java/com/trademo/app/TrademoApplication.java
@@ -0,0 +1,13 @@
+package com.trademo.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrademoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrademoApplication.class, args);
+	}
+
+}
