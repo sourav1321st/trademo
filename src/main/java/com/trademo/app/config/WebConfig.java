4c176567c239a59970d17764d52d1955222786c4
sourav1321st <souravmeher983@gmail.com>
2025-09-17 14:08:58 +0530

    @SuppressWarnings Annotation

--- src/main/java/com/trademo/app/config/WebConfig.java
+++ src/main/java/com/trademo/app/config/WebConfig.java
@@ -6,6 +6,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    @SuppressWarnings("null")
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         // Allow your frontend dev servers to call the API
